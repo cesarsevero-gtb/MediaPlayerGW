193fac4fc891e8dd86f771e42fa7115c97da6347
César severo <direito1boab@gmail.com>
2022-06-16 16:41:38 -0300

    Permisão ao Usurario ao arquivo interno criação de uma list View

--- app/src/main/java/com/getworld/mediaplayergw/MainActivity.java
+++ app/src/main/java/com/getworld/mediaplayergw/MainActivity.java
@@ -2,13 +2,58 @@ package com.getworld.mediaplayergw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] itens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        listView = (ListView) findViewById(R.id.listView);
+        retutimePermission();
+
+
     }
+
+    //Pedindo permisão para o usuário comando
+    public void retutimePermission()
+    {
+
+        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                    }
+                }).check();
+
+    }
+
+
+
 }
